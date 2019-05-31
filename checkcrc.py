aa77867ac8da62ca71045970ee36832965f8c35f
Daniil Zotkin <danya.zotkin@gmail.com>
2019-05-31 11:51:03 +0300

    Minor fixes
    1. Fix requirements.txt
    2. Fix offset in steps
    3. Adapt for python3

--- checkcrc.py
+++ checkcrc.py
@@ -6,11 +6,11 @@ extension = os.path.splitext(filename)[1][1:]
 print(hex(os.path.getsize(filename)))
 print extension
 with open(filename) as f:
-      while True:
-        c = f.read(1) #takes 20 bytes :D
+    while True:
+        c = f.read(1)  # takes 20 bytes :D
         if not c:
-          print "Update Over"
-          break
+            print "Update Over"
+            break
         cInt = int(c.encode('hex'), 16)
         crc = ((crc >> 8) | (crc << 8)) & 0xFFFF
         crc ^= (cInt & 0xff)
